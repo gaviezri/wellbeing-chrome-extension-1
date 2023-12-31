65061be3fde0218088c523e6c614def8c337cc91
Gaviezri <g.aviezri@gmail.com>
2023-12-31 11:57:37 +0200

    utilized local storage in app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import "./App.css";
 import Header from "./components/Header";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
 import Timers from "./components/Timer/Timers";
 
 function App() {
-  const [isOn, setIsOn] = useState(false);
+  const isAppOnKey = "isOn";
+  const storageValue = getStorageValueAsBoolean(localStorage.getItem(isAppOnKey));
+
+  const [isOn, setIsOn] = useState(storageValue);
+
+  useEffect(() => {
+    localStorage.setItem(isAppOnKey, isOn);
+  }, [isOn]);
+
+  useEffect(() => {
+    window.addEventListener("unload", () => {
+      // ! removed
+      localStorage.setItem(isAppOnKey, isOn);
+    });
+  }, []);
+
   return (
-    <>
+    <div className="App">
       <Header isOn={isOn} setIsOn={setIsOn} />
-      <Timers isOn={isOn} />
-    </>
+      {isOn && <Timers />}
+    </div>
   );
 }
 
 export default App;
+
+function getStorageValueAsBoolean(value) {
+  return value === "true";
+}
