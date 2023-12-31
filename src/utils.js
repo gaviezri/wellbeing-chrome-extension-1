9e1f1fe1b3cce25ed8351ecf109121f39fb2105c
Gaviezri <g.aviezri@gmail.com>
2023-12-31 14:43:31 +0200

    updated utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-export function getEpochTime() {
+export function getEpochTimeInSeconds() {
   return Math.floor(new Date().getTime() / 1000);
 }
 
@@ -23,3 +23,28 @@ export const strechingIntervalMarks = [
   // {value:3.5,label:3.5},
   { value: 4, label: 4 },
 ];
+
+export function formatTime(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedHours = String(hours).padStart(2, "0");
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
+export function convertValueToSeconds(value, hrs) {
+  const hourFactor = 3600;
+  const minuteFactor = 60;
+  const conversionFactor = hrs ? hourFactor : minuteFactor;
+  return value * conversionFactor;
+}
+
+export function calculateRemainingTimeBasedOnIntervalAndStartTime(startTimeInSeconds, intervalLength, hrs){
+  const time = (getEpochTimeInSeconds() - startTimeInSeconds) %  convertValueToSeconds(intervalLength, hrs)
+  console.log(time)
+  return time
+}
